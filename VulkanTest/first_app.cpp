bb436afbfda269abd07e8c1b7744c147f6340515
Isaac Marovitz <isaacryu@icloud.com>
2022-10-05 01:38:38 -0400

    Better normals

--- VulkanTest/first_app.cpp
+++ VulkanTest/first_app.cpp
@@ -58,11 +58,18 @@ namespace lve {
     void FirstApp::loadGameObjects() {
         std::shared_ptr<LveModel> lveModel = LveModel::createModelFromFile(lveDevice, "models/flat_vase.obj");
 
-        auto gameObject = LveGameObject::createGameObject();
-        gameObject.model = lveModel;
-        gameObject.transform.translation = {.0f, .5f, 2.5f};
-        gameObject.transform.scale = glm::vec3(3.f);
+        auto flatVase = LveGameObject::createGameObject();
+        flatVase.model = lveModel;
+        flatVase.transform.translation = {-.5f, .5f, 2.5f};
+        flatVase.transform.scale = {3.f, 1.5f, 3.f};
+        gameObjects.push_back(std::move(flatVase));
 
-        gameObjects.push_back(std::move(gameObject));
+        lveModel = LveModel::createModelFromFile(lveDevice, "models/smooth_vase.obj");
+
+        auto smoothVase = LveGameObject::createGameObject();
+        smoothVase.model = lveModel;
+        smoothVase.transform.translation = {.5f, .5f, 2.5f};
+        smoothVase.transform.scale = {3.f, 1.5f, 3.f};
+        gameObjects.push_back(std::move(smoothVase));
     }
 }
