3132f3950b301cd8ce3c8ea6c6084229e4dd0a2b
Isaac Marovitz <isaacryu@icloud.com>
2022-10-05 20:53:42 -0400

    Spinnyy

--- VulkanTest/first_app.cpp
+++ VulkanTest/first_app.cpp
@@ -17,14 +17,6 @@
 #include <chrono>
 
 namespace lve {
-    struct GlobalUbo {
-        glm::mat4 projection{1.f};
-        glm::mat4 view{1.f};
-        glm::vec4 ambientLightColor{1.f, 1.f, 1.f, .02f};
-        glm::vec3 lightPosition{-1.f};
-        alignas(16) glm::vec4 lightColor{1.f};
-    };
-
     FirstApp::FirstApp() {
         globalPool = LveDescriptorPool::Builder(lveDevice)
             .setMaxSets(LveSwapChain::MAX_FRAMES_IN_FLIGHT)
@@ -108,6 +100,7 @@ namespace lve {
                 GlobalUbo ubo{};
                 ubo.projection = camera.getProjection();
                 ubo.view = camera.getView();
+                pointLightSystem.update(frameInfo, ubo);
                 uboBuffers[frameIndex]->writeToBuffer(&ubo);
                 uboBuffers[frameIndex]->flush();
 
@@ -126,15 +119,36 @@ namespace lve {
         std::shared_ptr<LveModel> lveModel = LveModel::createModelFromFile(lveDevice, "models/monkey.obj");
         auto flatVase = LveGameObject::createGameObject();
         flatVase.model = lveModel;
-        flatVase.transform.translation = {0.f, 0.f, 2.5f};
-        flatVase.transform.scale = {2.f, 2.f, 2.f};
+        flatVase.transform.translation = {0.f, 0.f, 0.f};
+        flatVase.transform.scale = {1.5f, 1.5f, 1.5f};
         gameObjects.emplace(flatVase.getId(), std::move(flatVase));
 
         lveModel = LveModel::createModelFromFile(lveDevice, "models/quad.obj");
         auto floor = LveGameObject::createGameObject();
         floor.model = lveModel;
-        floor.transform.translation = {0.f, 2.f, 2.5f};
+        floor.transform.translation = {0.f, 2.f, 0.f};
         floor.transform.scale = {3.f, 3.f, 3.f};
         gameObjects.emplace(floor.getId(), std::move(floor));
+
+        std::vector<glm::vec3> lightColors {
+            {1.f, .1f, .1f},
+            {.1f, .1f, 1.f},
+            {.1f, 1.f, .1f},
+            {1.f, 1.f, .1f},
+            {.1f, 1.f, 1.f},
+            {1.f, 1.f, 1.f}
+        };
+
+        for (int i = 0; i < lightColors.size(); i++) {
+            auto pointLight = LveGameObject::makePointLight(0.2f);
+            pointLight.color = lightColors[i];
+            auto rotateLight = glm::rotate(
+                glm::mat4(1.f), 
+                (i * glm::two_pi<float>()) / lightColors.size(),
+                {0.f, -1.f, 0.f});
+            pointLight.transform.translation = glm::vec3(rotateLight * glm::vec4(-1.f, -1.f, -1.f, 1.f));
+            gameObjects.emplace(pointLight.getId(), std::move(pointLight));
+        }
+        
     }
 }
--- VulkanTest/lve_frame_info.hpp
+++ VulkanTest/lve_frame_info.hpp
@@ -14,4 +14,19 @@ namespace lve {
         VkDescriptorSet globalDescriptorSet;
         LveGameObject::Map &gameObjects;
     };
+
+    #define MAX_LIGHTS 10
+
+    struct PointLight {
+        glm::vec4 position{};
+        glm::vec4 color{};
+    };
+
+    struct GlobalUbo {
+        glm::mat4 projection{1.f};
+        glm::mat4 view{1.f};
+        glm::vec4 ambientLightColor{1.f, 1.f, 1.f, .02f};
+        PointLight pointLights[MAX_LIGHTS];
+        int numLights;
+    };
 }
